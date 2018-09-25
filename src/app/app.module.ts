9c83bad8f03b87db92aa33d5dc48e0b0b3bd6c8d
kwathek <w.kahla16035@pi.tn>
2018-09-25 13:39:49 +0100

    creation des routes et declaration de deux components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {LoginModule} from './login/login.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginPage2Component } from './login-page2/login-page2.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'LoginComplete',
+    component: LoginPage2Component
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginPage2Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
