a0e089b60bb4507909e399ebd7c8783f89f1aa20
kwathek <w.kahla16035@pi.tn>
2018-09-25 14:40:43 +0100

    ajout d'un listener sur le scroll + verification checkbox et validation

--- src/app/login-page2/login-page2.component.ts
+++ src/app/login-page2/login-page2.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { ScrollEvent } from 'ngx-scroll-event';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page2',
   templateUrl: './login-page2.component.html',
-  styleUrls: ['./login-page2.component.scss']
+  styleUrls: ['./login-page2.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginPage2Component implements OnInit {
-
-  constructor() { }
+  disabled = 'true';
+  marked = 'false';
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  // vérification si le checkbox est checked
+  toggleVisibility(e) {
+    this.marked = e.target.checked;
+  }
+  // Listener qui verifie le Scroll
+  public handleScroll(event: ScrollEvent) {
+    if (event.isReachingBottom) {
+      console.log(`the user is reaching the bottom`);
+      this.disabled = 'false';
+    }
+  }
+  // validation et navigation vers une autre page
+  valider() {
+    this.router.navigateByUrl('/');
+  }
 }
