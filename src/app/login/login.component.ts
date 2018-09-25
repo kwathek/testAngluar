e40b9893258d2b00bd8dcd6df06789e75aa96a7d
kwathek <w.kahla16035@pi.tn>
2018-09-25 13:35:04 +0100

    login component + tryLogin function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {ApiService} from './api.service';
+import {CustomerService} from './customer.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor() { }
+  email = 'peter@klaven';
+  password = 'cityslicka';
 
-  ngOnInit() {
+  constructor(private api: ApiService, private customer: CustomerService, private router: Router) {
+  }
+
+  tryLogin() {
+    if (this.email === 'peter@klaven' && this.password === 'cityslicka') {
+      this.api.login(
+        this.email,
+        this.password
+      )
+        .subscribe(
+          r => {
+            if (r.token) {
+              this.customer.setToken(r.token);
+              this.router.navigateByUrl('/LoginComplete');
+            }
+          },
+          r => {
+            alert(r.error.error);
+          });
+    } else if (this.email !== 'peter@klaven' && this.password === 'cityslicka') {
+      alert(' vérifier votre email !');
+    } else if (this.email === 'peter@klaven' && this.password !== 'cityslicka') {
+      alert(' vérifier votre mot de passe !');
+    } else {
+      alert(' vérifier votre email et mot de passe ');
+    }
   }
 
 }
