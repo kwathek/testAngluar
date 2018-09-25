a92b7f18aeee4079bed14ff81097135e67f4fd03
kwathek <w.kahla16035@pi.tn>
2018-09-25 16:20:09 +0100

    add AuthService

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import {AuthService} from './auth.service';
 
 @NgModule({
   imports: [
@@ -9,6 +10,7 @@ import { LoginComponent } from './login.component';
     FormsModule
   ],
   declarations: [LoginComponent],
+  providers: [AuthService],
   exports: [
     LoginComponent
   ]
