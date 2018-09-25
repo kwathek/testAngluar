cbb21494203dc79091a9bd59d180012a6a694df3
kwathek <w.kahla16035@pi.tn>
2018-09-25 17:02:22 +0100

    condition sur l'utilisateur

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,17 +2,19 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 import { AuthService } from './login/auth.service';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
+import {CustomerService} from './login/customer.service';
+import {and} from '@angular/router/src/utils/collection';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router, private customer: CustomerService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if ( this.authService.isAuth) {
+    if ( this.authService.isAuth || this.customer.isLogged()) {
       return true;
     } else {
       this.router.navigateByUrl('login');
